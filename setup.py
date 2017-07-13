b7b7b5d30d9750695c6576e952d9aaf4a2841283
Josip Kotarac <jkotarac@extensionengine.com>
2017-07-13 13:14:08 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='edx-score-grade-api',
-    version='0.0.1',
+    version='0.1.6',
     description='edx-score-grade-api Score Grade API',
     license='Affero GNU General Public License v3 (GPLv3)',
     url="https://github.com/kotky/edx-score-api",
-    author="kotky",
+    author="Josip Kotarac",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
